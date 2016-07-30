42e35aacb0ea6835c7e6bb099c800682fd880dc8
Rodger Benham <rodgerbenham@10-76-1-43.wifi-f.deakin.edu.au>
2016-07-30 17:20:23 +1000

    Parsed and working data of all data with roof heights

--- parser.rb
+++ parser.rb
@@ -1,18 +1,34 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
+require 'csv'
 
-icount = 0 
-xmlfeed = Nokogiri::XML(open("test.xml"))
-items = xmlfeed.xpath("//item")
-items.each do |item|
-  text = item.children.children.first.text  
-  if ( text =~ /99/ )
-    icount += 1
+@doc = Nokogiri::XML.parse(open("Geelong.kml"))
+@doc.remove_namespaces!
+
+items = @doc.xpath('//Placemark')
+id = 0
+
+CSV.open("results.csv", "w") do |csv|
+  items.each do |placemark|
+    extended = placemark.css('ExtendedData')
+    extended.each do |eData|
+      simple = eData.css('SimpleData').css('SimpleData')
+      if simple.count != 0 
+        simple.each do |simpledata|
+          if simpledata.attr('name') == 'ROOF_HT'
+            # Great! This datapoint has a roof height associated with it.
+            roof_height = simpledata.children[0].to_s
+
+            # What is it's coordinates?
+            longitude = placemark.css('longitude').children[0].to_s
+            latitude = placemark.css('latitude').children[0].to_s
+            csv << [id, longitude, latitude, roof_height] 
+            id += 1
+          end
+        end
+      end
+    end
   end
 end
-
-othercount = xmlfeed.xpath("//totalcount").inner_text.to_i - icount 
-
-puts icount
-puts othercount
